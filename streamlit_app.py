2a7b54ab7364eb23b085090e19f7a6988506ab1a
NeliniJ <113869552+NeliniJ@users.noreply.github.com>
2023-11-18 20:13:10 +1000

    Update streamlit_app.py
    removing json formatted text

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json()) - since we have data nicely in a table below, removing regular json formatted text
 
 # take the json version of the response and making into look regular / nice
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
